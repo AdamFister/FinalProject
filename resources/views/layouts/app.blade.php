188c0156ee51dbbc259b46fc3bf0b5bc373d9a45
Adam Fister <arfister@gmail.com>
2019-05-09 08:12:06 -0400

    changed title in layout app to Local Jam

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Local Jam') }}</title>
+    <title>Local Jam</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
